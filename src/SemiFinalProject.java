737570c76234c162ff8db6c5f982a7af9c52668f
Nico <Nicollosa35@gmail.com>
2022-09-01 19:11:40 +0800

    Second commit

--- src/SemiFinalProject.java
+++ src/SemiFinalProject.java
@@ -2,33 +2,20 @@
 import java.util.*;
 
 public class SemiFinalProject {
-//<!DOCTYPE html>
-//<html>
-//<body>
-//<h1>My First Heading</h1>
-//<p>My first paragraph.</p>
-//</body>
-//</html>
     public static void main(String args[])
     {
         Scanner s = new Scanner(System.in);
         Stack<String> STACK = new Stack();
-
-
-        boolean isComplete = true;
-        while (isComplete){
+        int x = 1;
+        boolean isComplete = false;
+        while (!isComplete){
 
             System.out.println("Enter 7 lines of code:");
-            String.valueOf(STACK.add(s.nextLine()));
-            String.valueOf(STACK.add(s.nextLine()));
-            String.valueOf(STACK.add(s.nextLine()));
-            String.valueOf(STACK.add(s.nextLine()));
-            String.valueOf(STACK.add(s.nextLine()));
-            String.valueOf(STACK.add(s.nextLine()));
-            String.valueOf(STACK.add(s.nextLine()));
-
-            //         System.out.println("hello");
-
+            while(x <= 7 ) {
+                STACK.add(s.nextLine());
+                x++;
+            }
+            x = 1;
             if (STACK.get(1).equals("</html>")  &&
                     STACK.get(0).equals("<!DOCTYPE html>") && STACK.get(2).equals("<body>")
                     && STACK.get(3).contains("<h1>") && STACK.get(3).contains("</h1>")
@@ -42,34 +29,23 @@ public class SemiFinalProject {
                 for (int i = 0; i <= 6; i++) {
                     System.out.println(j + "\t"+ STACK.get(i)  );
                     j++;
-                    isComplete = false;
+                    isComplete = true;
                 }
-
             }else{
                 System.out.println("YOUR CODE IS INVALID OR INCOMPLETE!");
-
                 int j = 1;
 
                 for (int i = 0; i <= 6; i++) {
                     System.out.println(j + "\t" + STACK.get(i));
                     j++;
-
                 }
                 System.out.println("--------------------------------");
-
-                STACK.pop();
-                STACK.pop();
-                STACK.pop();
-                STACK.pop();
-                STACK.pop();
-                STACK.pop();
-                STACK.pop();
-
+                for (int n = 0; n < 7 ; n++) {
+                    STACK.pop();
+                }
             }
-
         }
     }
-
 }
 //<!DOCTYPE html>
 //<html>
